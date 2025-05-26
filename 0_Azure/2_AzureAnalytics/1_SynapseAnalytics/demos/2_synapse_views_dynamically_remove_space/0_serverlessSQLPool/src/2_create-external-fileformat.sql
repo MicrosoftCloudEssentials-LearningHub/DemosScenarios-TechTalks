849fa79b63525dddf4e998547200215483de7164
Timna Brown <24630902+brown9804@users.noreply.github.com>
2025-05-25 18:45:21 -0600

    moved to repo

--- 0_Azure/2_AzureAnalytics/1_SynapseAnalytics/demos/2_synapse_views_dynamically_remove_space/0_serverlessSQLPool/src/2_create-external-fileformat.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTERNAL FILE FORMAT MyFileFormat
-WITH (
-    FORMAT_TYPE = DELIMITEDTEXT,
-    FORMAT_OPTIONS (
-        FIELD_TERMINATOR = ',',
-        STRING_DELIMITER = '"',
-        FIRST_ROW = 2
-    )
-);
